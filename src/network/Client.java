0b935c4525dc97648b5cf5b3d13f19ea2a47babe
jaekw <jae-kwen@daum.net>
2025-01-14 17:32:08 +0900

    refactor : studying

--- src/network/Client.java
+++ src/network/Client.java
@@ -14,29 +14,27 @@ public class Client {
 
     public static void main(String[] args) throws IOException {
 
-        Socket socket = new Socket("localhost", PORT);
-        DataInputStream dis = new DataInputStream(socket.getInputStream());
-        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+        try (Socket socket = new Socket("localhost", PORT);
+             DataInputStream dis = new DataInputStream(socket.getInputStream());
+             DataOutputStream dos = new DataOutputStream(socket.getOutputStream())) {
 
-        Scanner sc = new Scanner(System.in);
+            Scanner sc = new Scanner(System.in);
+            while (true) {
+                //to 서버
+                String toSend = sc.nextLine();
+                dos.writeUTF(toSend);
+                System.out.println("client -> server : " + toSend);
+                if(toSend.equals("exit")){
+                    break;
+                }
+                //from 서버
+                String received = dis.readUTF();
+                System.out.println("server -> client : " + received);
 
-        while (true) {
-
-            //to 서버
-            String toSend = sc.nextLine();
-            dos.writeUTF(toSend);
-            System.out.println("client -> server : " + toSend);
-
-            if(toSend.equals("exit")){
-                break;
-            }
-
-            //from 서버
-            String received = dis.readUTF();
-            System.out.println("server -> client : " + received);
+                
+        } catch (IOException e) {
 
         }
-
         dis.close();
         dos.close();
         socket.close();
